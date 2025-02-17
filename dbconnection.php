26379fc03150ddf9dbff2b177e032c05652ec03e
isybob2 <isybobwil3@gmail.com>
2025-02-17 09:20:18 +0100

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -5,12 +5,11 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','damsmsdb');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+
+$database= new mysqli($host,$username,$password,$db_name);
+
+  if ($database->connect_error){
+        exit("Error: " . $e->getMessage());
+    }
+
 ?>
